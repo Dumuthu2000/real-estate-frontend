743083ea1bfb549d800b59153a0dae7d879573cd
Dumuthu Jayasinghe <dumuthujayasinghe@gmail.com>
2024-07-06 22:05:32 +0530

    Image upload for user in firebase

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,12 +1,66 @@
+import { useRef, useState, useEffect } from "react";
 import { useSelector } from "react-redux"
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from "../firebase";
 
 const Profile = () => {
+  const[file, setFile] = useState(undefined);
+  const[uploadPercentage, setUploadPercentage] = useState(0);
+  const[fileUploadError, setFileUploadError] = useState(false);
+  const[formData, setFormData] = useState({});
   const{ currentUser } = useSelector(state => state.user);
+  const fileRef = useRef(null);
+
+  useEffect(()=>{
+    if(file){
+      handleFileUpload(file);
+    }
+  },[file]);
+
+  const handleFileUpload=(file)=>{
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_change',
+      (snapshot)=>{
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadPercentage(Math.round(progress));
+      },
+      (error)=>{
+        setFileUploadError(true);
+      },
+      ()=>{
+        getDownloadURL(uploadTask.snapshot.ref)
+        .then((downloadURL)=>{
+          setFormData((prev) => ({ ...prev, avatar: downloadURL }));
+        })
+      })
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
-        <img src={currentUser.avatar} alt="Profile" className="w-24 h-24 rounded-full object-cover cursor-pointer self-center mt-2"/>
+        <input
+         onChange={(e)=>{
+          setFile(e.target.files[0]);
+         }}
+         type="file" ref={fileRef} hidden accept="image/.*"/>
+        <img src={formData.avatar || currentUser.avatar} alt="Profile" className="w-24 h-24 rounded-full object-cover cursor-pointer self-center mt-2"
+        onClick={()=>{
+          fileRef.current.click()
+        }}/>
+        <p className="text-center">
+          {
+            fileUploadError ? 
+            <span className="text-red-700">Error image upload (Image must be less than 2MB)</span> :
+            uploadPercentage > 0 && uploadPercentage < 100 ?
+            <span className="text-slate-700">{`Uploading ${uploadPercentage}%`}</span> :
+            uploadPercentage === 100 ?
+            <span className="text-green-700">Image uploaded successfully</span> : null
+          }
+        </p>
         <input type="text" placeholder="username" id="username"
         className="border p-3 rounded-lg"/>
         <input type="text" placeholder="email" id="email"
--- src/firebase.js
+++ src/firebase.js
@@ -1,32 +1,13 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app"
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAR9PLtOHp6kzTxOM3NiKagtoHvq2itMCo",
-//   // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: "ceylon-estate.firebaseapp.com",
-//   projectId: "ceylon-estate",
-//   storageBucket: "ceylon-estate.appspot.com",
-//   messagingSenderId: "744928853410",
-//   appId: "1:744928853410:web:afb42fd052c3d452fe86fb"
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-
-import { initializeApp } from "firebase/app";
-// import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAR9PLtOHp6kzTxOM3NiKagtoHvq2itMCo",
-  authDomain: "socio-estate.firebaseapp.com",
-  projectId: "socio-estate",
-  storageBucket: "socio-estate.appspot.com",
-  messagingSenderId: "1029894639268",
-  appId: "1:1029894639268:web:5ff5c3d61ed5d4b03ce031"
+  apiKey: "AIzaSyC9Kk56yezyU4OWioPP02dDIsA6kMii_0M",
+  authDomain: "real-estate-b37fb.firebaseapp.com",
+  projectId: "real-estate-b37fb",
+  storageBucket: "real-estate-b37fb.appspot.com",
+  messagingSenderId: "247211908642",
+  appId: "1:247211908642:web:e1d3745b38272d632b747b"
 };
 
 // Initialize Firebase
